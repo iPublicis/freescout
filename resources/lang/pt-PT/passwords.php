225952aca61852dc447c1a647ca8d3d5c605edf9
Lopo <dev@ipublicis.com>
2021-01-26 15:18:53 +0000

    Update passwords.php
    Not properly set European Portuguese

--- resources/lang/pt-PT/passwords.php
+++ resources/lang/pt-PT/passwords.php
@@ -14,7 +14,7 @@ return [
 
     'password' => 'A senha deve conter pelo menos seis caracteres e ser igual à confirmação.',
     'reset'    => 'Sua senha foi redefinida!',
-    'sent'     => 'Enviamos um link para redefinir a sua senha por e-mail.',
+    'sent'     => 'Enviámos por email uma ligação para poder redefinir a sua senha.',
     'token'    => 'Esse código de redefinição de senha é inválido.',
-    'user'     => 'Não conseguimos encontrar nenhum usuário com o endereço de e-mail informado.',
+    'user'     => 'Não conseguimos encontrar nenhum utilizador com o endereço de e-mail inserido.',
 ];
